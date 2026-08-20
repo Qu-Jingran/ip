2c26fc73f0bdd9cfa7ab720c36f339585ed7bed1
x_q_h <254371291+Qu-Jingran@users.noreply.github.com>
2026-08-21 03:00:50 +0800

    Level 3. Mark as Done

--- src/main/java/Eli.java
+++ src/main/java/Eli.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Eli {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String[] arr = new String[100];
+        Task[] arr = new Task[100];
         int count = 0;
         String banner = " _______     _           _____ \n"
                 + "|  _____|   | |         |_   _|\n"
@@ -17,20 +17,39 @@ public class Eli {
                         "你需要什么帮助？\n" +
                         "\n____________________________________________________________\n");
 
-        for(String s = input.next(); !s.equals("bye") && !s.equals("再见"); s = input.next()){
+        for (String s = input.nextLine().trim();
+             !s.equals("bye") && !s.equals("再见");
+             s = input.nextLine().trim()) {
             String list = "";
 
             if(s.equals("list")) {
-                for(int i = 0; arr[i] != null; i++) {
-                    list += i+1 + ". " + arr[i] + "\n";
+                for (int i = 0; arr[i] != null; i++) {
+                    list += i + 1 + ". " + arr[i].toString() + "\n";
                 }
                 System.out.println(list);
+
+            } else if(s.startsWith("mark")) {
+                int taskNumber = Integer.parseInt(s.substring(5).trim());
+                if (taskNumber > count) {
+                    System.out.println("Sorry, 我们没有那么多任务");
+                } else {
+                    arr[taskNumber - 1].markAsDone();
+                    System.out.println("Nice! 已经标记为完成了");
+                }
+            } else if(s.startsWith("unmark")) {
+                int taskNumber = Integer.parseInt(s.substring(7).trim());
+                if (taskNumber > count) {
+                    System.out.println("Sorry, 我们没有那么多任务");
+                System.out.println("Nice! 已经标记为完成了");
+                } else {
+                    arr[taskNumber - 1].unMark();
+                    System.out.println("ok!已经标记为未完成");
+                }
 
             }
             else {
                 System.out.printf("added: %s%n", s);
-
-                arr[count] = s;
+                arr[count] = new Task(s);
                 count++;
             }
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,31 @@
+public class Task {
+        protected String description;
+        protected boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public String getStatusIcon() {
+            return (isDone ? "X" : " "); // mark done task with X
+        }
+
+        public void markAsDone() {
+            this.isDone = true;
+        }
+
+        public void unMark() {
+            this.isDone = false;
+
+        }
+
+        @Override
+        public String toString() {
+            String temp = "[" + this.getStatusIcon() + "] " + this.description;
+            return temp;
+        }
+
+
+
+}
